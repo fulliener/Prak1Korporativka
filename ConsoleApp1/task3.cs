d6381a54a0b796ac5330260353a2ce4f5c279380
fulliener <ruslanakperov80857@gmail.com>
2024-02-24 19:27:31 +0300

    Initial commit

---
+++ ConsoleApp1/task3.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class Program
+{
+    static void Main()
+    {
+        // Прочитайте данные из файла JSON
+        string jsonFilePath = "D:\\korporaTIV\\Новая папка\\ConsoleApp1\\ConsoleApp1\\data.json";
+        string jsonData = File.ReadAllText(jsonFilePath);
+
+        // Десериализуем JSON в объекты C#, проверяя на null
+        var data = JsonSerializer.Deserialize<Data>(jsonData);
+        if (data == null)
+        {
+            Console.WriteLine("Ошибка: Не удалось прочитать данные из файла JSON или данные отсутствуют.");
+            return;
+        }
+
+        // Проверяем, что список дисциплин с оценками не равен null
+        if (data.StudyPlan == null)
+        {
+            Console.WriteLine("Ошибка: Список дисциплин с оценками отсутствует.");
+            return;
+        }
+
+        // Код студента, для которого нужно добавить оценку
+        string studentCode = "S001";
+
+        // Код предмета и оценка, которую необходимо добавить
+        string subjectCode = "SUB006";
+        int grade = 5;
+
+        // Проверяем, есть ли уже оценка для указанного студента и предмета
+        var existingGrade = data.StudyPlan.FirstOrDefault(entry => entry?.StudentCode == studentCode && entry?.SubjectCode == subjectCode);
+
+        if (existingGrade != null)
+        {
+            Console.WriteLine($"Оценка для студента с кодом {studentCode} по предмету с кодом {subjectCode} уже существует.");
+            return;
+        }
+
+        // Добавляем новую оценку в список дисциплин с оценкой
+        if (data.StudyPlan == null)
+        {
+            data.StudyPlan = new List<StudyPlanEntry>(); // Убедимся, что список инициализирован перед добавлением элемента
+        }
+        data.StudyPlan.Add(new StudyPlanEntry { StudentCode = studentCode, SubjectCode = subjectCode, Grade = grade });
+
+        // Сериализуем обновленные данные обратно в JSON
+        string updatedJsonData = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+
+        // Записываем сериализованные данные обратно в файл
+        File.WriteAllText(jsonFilePath, updatedJsonData);
+
+        Console.WriteLine($"Новая оценка {grade} добавлена для студента с кодом {studentCode} по предмету с кодом {subjectCode}.");
+    }
+}
+
+class Data
+{
+    [JsonPropertyName("students")]
+    public List<Student> Students { get; set; }
+    
+    [JsonPropertyName("subjects")]
+    public List<Subject> Subjects { get; set; }
+    
+    [JsonPropertyName("study_plan")]
+    public List<StudyPlanEntry> StudyPlan { get; set; }
+}
+
+class Student
+{
+    [JsonPropertyName("student_code")]
+    public string StudentCode { get; set; }
+    
+    [JsonPropertyName("last_name")]
+    public string LastName { get; set; }
+    
+    [JsonPropertyName("first_name")]
+    public string FirstName { get; set; }
+    
+    [JsonPropertyName("middle_name")]
+    public string MiddleName { get; set; }
+}
+
+class Subject
+{
+    [JsonPropertyName("subject_code")]
+    public string SubjectCode { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    
+    [JsonPropertyName("lecture_hours")]
+    public int LectureHours { get; set; }
+    
+    [JsonPropertyName("practice_hours")]
+    public int PracticeHours { get; set; }
+}
+
+class StudyPlanEntry
+{
+    [JsonPropertyName("student_code")]
+    public string StudentCode { get; set; }
+    
+    [JsonPropertyName("subject_code")]
+    public string SubjectCode { get; set; }
+    
+    [JsonPropertyName("grade")]
+    public int Grade { get; set; }
+}
---
+++ ConsoleApp2/task4.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+class task4
+{
+    static void Main()
+    {
+        // Загрузка данных из JSON-файла
+        var jsonData = File.ReadAllText("D:\\korporaTIV\\Новая папка\\ConsoleApp1\\ConsoleApp2\\data.json");
+
+        // Десериализация JSON в объекты C#
+        var data = JsonSerializer.Deserialize<JsonNode>(jsonData);
+
+        // Код студента, для которого нужно сформировать список дисциплин с оценкой
+        string studentCode = "S001";
+
+        // Получение списка дисциплин с оценкой для указанного студента
+        var studyPlan = (JsonArray)data["study_plan"];
+        var subjects = (JsonArray)data["subjects"];
+
+        var result = new List<(string, int)>();
+
+        foreach (var entry in studyPlan)
+        {
+            var studentCodeEntry = (string)((JsonObject)entry)["student_code"];
+            var subjectCode = (string)((JsonObject)entry)["subject_code"];
+            var grade = (int)((JsonObject)entry)["grade"];
+
+            if (studentCodeEntry == studentCode)
+            {
+                var subjectName = ((JsonObject)subjects.First(s => (string)((JsonObject)s)["subject_code"] == subjectCode))["name"];
+                result.Add(((string)subjectName, grade));
+            }
+        }
+
+        // Вывод результата
+        Console.WriteLine($"Список дисциплин с оценкой для студента с кодом {studentCode}:");
+        foreach (var item in result)
+        {
+            Console.WriteLine($"Дисциплина: {item.Item1}, Оценка: {item.Item2}");
+        }
+
+        // Подсчет процента оценок
+        var excellentCount = result.Count(grade => grade.Item2 == 5);
+        var goodCount = result.Count(grade => grade.Item2 == 4);
+        var satisfactoryCount = result.Count(grade => grade.Item2 == 3);
+        var totalCount = result.Count;
+
+        // Вывод процента оценок
+        Console.WriteLine($"Процент оценок:");
+        Console.WriteLine($"Отлично: {(double)excellentCount / totalCount * 100}%");
+        Console.WriteLine($"Хорошо: {(double)goodCount / totalCount * 100}%");
+        Console.WriteLine($"Удовлетворительно: {(double)satisfactoryCount / totalCount * 100}%");
+    }
+}
